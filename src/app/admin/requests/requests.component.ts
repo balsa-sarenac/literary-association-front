069d71b39064a744f579f1bc07ea14bae3a52a0f
Balsa <balsa.smi15@gmail.com>
2020-12-12 12:27:12 +0100

    Create admin service and request interface

--- src/app/admin/requests/requests.component.ts
+++ src/app/admin/requests/requests.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { AdminService } from '../shared/admin.service';
+import { IUserRequest } from '../shared/iuser-request.registration';
 
 @Component({
-  selector: 'app-requests',
-  templateUrl: './requests.component.html',
-  styleUrls: ['./requests.component.css']
+	selector: 'app-requests',
+	templateUrl: './requests.component.html',
+	styleUrls: ['./requests.component.css'],
 })
 export class RequestsComponent implements OnInit {
+	users: IUserRequest[] = [];
 
-  constructor() { }
+	constructor(private adminService: AdminService) {}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.adminService.getRequests().subscribe((data: IUserRequest[]) => (this.users = data));
+	}
 }
