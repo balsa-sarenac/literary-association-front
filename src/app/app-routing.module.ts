711a5c4ee287232743dd2cb29bd0ee161147c091
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-13 20:30:26 +0100

    Added register route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { HomeAdminComponent } from './admin/home-admin/home-admin.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
 	{
 		path: 'welcome',
 		component: HomepageComponent,
-		children: [{ path: 'login', component: LoginComponent }],
+		children: [{ path: 'login', component: LoginComponent },
+					{path: 'register', component: RegisterComponent}],
 	},
 	{
 		path: 'admin',
