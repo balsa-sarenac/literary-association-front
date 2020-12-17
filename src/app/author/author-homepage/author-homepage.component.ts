76b9f75c513e1a53ccce4a2c1f8d00f4949c6fb2
vtanja <51333448+vtanja@users.noreply.github.com>
2020-12-17 19:37:30 +0100

    author-homepage updated to render generic form

--- src/app/author/author-homepage/author-homepage.component.ts
+++ src/app/author/author-homepage/author-homepage.component.ts
@@ -11,39 +11,20 @@ export class AuthorHomepageComponent implements OnInit {
 
   formFieldsDto = null;
   formFields:any[] = [];
-  taskId='';
+  processId:string;
   fileToUpload: File ;
   uploadForm:FormGroup=new FormGroup({});
 
   constructor(private authorService:AuthorService) { 
     this.fileToUpload = null;
 
-    this.authorService.loadForm().subscribe(res=>{
-      this.formFieldsDto = res;
-      this.formFields = res.formFieldList;
-      this.taskId = res.taskId;
-      console.log(this.formFields);
-      
-  this.formFields.forEach((element:any)=>{
-      let fc = new FormControl('');
-
-      console.log(element);
-      let validators:any[]=[];
-      element.validationConstraints.map((validator:any)=>{
-          if(validator.name == 'required'){
-            validators.push(Validators.required);
-          }
-      })
-      console.log(validators);
-      fc.setValidators(validators);
-
-      console.log(fc);
-      this.uploadForm.addControl(element.id, fc);
-    })
-  })
+    this.processId = this.authorService.startProcess();
   }
 
   ngOnInit(): void {
+    this.authorService.loadForm(this.processId).subscribe((res)=>{
+
+    });
   }
 
   handleFileInput(event) {
@@ -61,33 +42,33 @@ export class AuthorHomepageComponent implements OnInit {
 
   onSubmit(value:any){
 
-    var author:{id:string, value:File}=null;
+    // var author:{id:string, value:File}=null;
 
-    for (var property in value) {
-      console.log(property);
-      console.log(value[property]);
-      author= {id:property, value:this.fileToUpload};
-    }
+    // for (var property in value) {
+    //   console.log(property);
+    //   console.log(value[property]);
+    //   author= {id:property, value:this.fileToUpload};
+    // }
 
-    console.log(author);
+    // console.log(author);
 
-      if(this.formFieldsDto !== null){
-        let x = this.authorService.submitDocuments(author, this.taskId);
+    //   if(this.formFieldsDto !== null){
+    //     let x = this.authorService.submitDocuments(author, this.processId);
 
-        x.subscribe(
-          res => {
-            console.log(res);
+    //     x.subscribe(
+    //       res => {
+    //         console.log(res);
             
-            alert("You have successfully uploaded documents!")
+    //         alert("You have successfully uploaded documents!")
 
-            //this.router.navigate(['welcome'])
+    //         //this.router.navigate(['welcome'])
 
-          },
-          err => {
-            console.log(err.message);
-          }
-        );
-      }
+    //       },
+    //       err => {
+    //         console.log(err.message);
+    //       }
+    //     );
+    //   }
 
       
     }
