7b2852e6874bdb566c1c1ea0163b6ddff49dc11d
nikolina-sarenac <nina.sarenac@hotmail.com>
2020-12-25 19:14:33 +0100

    Print enum as select element

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -77,7 +77,7 @@ export class FormComponent implements OnInit {
 		console.log(res);
 
 		res.formFieldList.forEach((element:any) => {
-			if(element.type.name == "multiselect"){
+			if(element.type.name == "multiselect" || element.type.name == "enum"){
 				element.type.values = Object.values(element.type.values);
 			}
 		});
