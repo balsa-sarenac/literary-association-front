2fd9564a37507f29760abe383a72f51806d7e2ce
Balsa <balsa.smi15@gmail.com>
2021-01-15 23:35:49 +0100

    Preview single unpublished book to beta reader

---
+++ src/app/reader/unpublished-book/unpublished-book.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-unpublished-book',
+  templateUrl: './unpublished-book.component.html',
+  styleUrls: ['./unpublished-book.component.css']
+})
+export class UnpublishedBookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
