130782bd636dcf3776705ff86b4131c87d8fee1d
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-01-21 21:42:02 +0100

    Implement list component

---
+++ src/app/chief-editor/publishing-requests-list/publishing-requests-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/shared/auth.service';
+import { IPublishingRequest } from 'src/app/DTO/ipublishing-request';
+import { ChiefEditorService } from '../shared/chief-editor.service';
+
+@Component({
+  selector: 'app-publishing-requests-list',
+  templateUrl: './publishing-requests-list.component.html',
+  styleUrls: ['./publishing-requests-list.component.css']
+})
+export class PublishingRequestsListComponent implements OnInit {
+  requests: IPublishingRequest[] = [];
+
+  constructor(private authService:AuthService, private chiefEditorService:ChiefEditorService) { }
+
+  ngOnInit(): void {
+    this.refreshTable();
+  }
+
+  refreshTable() {
+    var logged=this.authService.getLoggedUser();
+		this.chiefEditorService.getRequests(logged).subscribe((data: IPublishingRequest[]) =>{this.requests = data; } );
+	}
+
+}
