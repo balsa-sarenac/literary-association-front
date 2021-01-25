1e0be2c64f47ca11138b9618d7bfd6190b60c29e
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-01-25 18:49:30 +0100

    Update routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,8 +27,8 @@ import { PublishingRequestComponent } from './chief-editor/publishing-request/pu
 import { LectorRequestsListComponent } from './lector/lector-request-list/lector-requests-list.component';
 import { LectorRequestComponent } from './lector/lector-request/lector-request.component';
 import { HomeLectorComponent } from './homepage/home-lector/home-lector.component';
-import {ComplaintsComponent} from './chief-editor/complaints/complaints.component';
-import {ComplaintInvestigationComponent} from './chief-editor/complaint-investigation/complaint-investigation.component';
+import { ComplaintsComponent } from './chief-editor/complaints/complaints.component';
+import { ComplaintInvestigationComponent } from './chief-editor/complaint-investigation/complaint-investigation.component';
 
 const routes: Routes = [
 	{ path: '', redirectTo: 'welcome', pathMatch: 'full' },
@@ -57,16 +57,16 @@ const routes: Routes = [
 		path: 'reader',
 		component: HomeReaderComponent,
 		children: [
-      { path: 'beta-books', component: BetaReaderBooksComponent },
-      { path: 'beta-books/:id', component: UnpublishedBookComponent }
-    ],
+			{ path: 'beta-books', component: BetaReaderBooksComponent },
+			{ path: 'beta-books/:id', component: UnpublishedBookComponent }
+		],
 	},
 	{
 		path: 'author',
 		component: HomeAuthorComponent,
 		children: [
 			{
-				path:'books',
+				path: 'books',
 				component: BookListComponent
 			},
 			{
@@ -79,12 +79,12 @@ const routes: Routes = [
 			},
 			{
 				path: 'requests/:id',
-			 	component: UploadBookComponent
-			 },
-			 {
+				component: UploadBookComponent
+			},
+			{
 				path: 'file-a-complaint/:id',
-			 	component: FileComplaintComponent
-			 },
+				component: FileComplaintComponent
+			},
 		],
 	},
 	{
@@ -106,8 +106,8 @@ const routes: Routes = [
 		],
 	},
 	{
-		path:'chief-editor',
-		component:HomeChiefEditorComponent,
+		path: 'chief-editor',
+		component: HomeChiefEditorComponent,
 		children: [
 			{
 				path: 'publishing-requests',
@@ -118,26 +118,18 @@ const routes: Routes = [
 				component: PublishingRequestComponent
 			},
 			{
-				path: 'publishing-requests',
-				component: PublishingRequestsComponent
+				path: 'complaints',
+				component: ComplaintsComponent
 			},
 			{
-				path: 'publishing-request/:id',
-				component: PublishingRequestComponent
-			},
-      {
-        path: 'complaints',
-        component: ComplaintsComponent
-      },
-      {
-        path: 'complaints/:id',
-        component: ComplaintInvestigationComponent
-      }
+				path: 'complaints/:id',
+				component: ComplaintInvestigationComponent
+			}
 		],
 	},
 	{
 		path: 'lector',
-		component:HomeLectorComponent,
+		component: HomeLectorComponent,
 		children: [
 			{
 				path: 'lector-requests',
@@ -155,4 +147,4 @@ const routes: Routes = [
 	imports: [RouterModule.forRoot(routes)],
 	exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
