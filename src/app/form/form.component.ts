19c56519da88ace47da4865bead9da9453ac2158
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-01-31 17:33:21 +0100

    Route lector and editor after form submission

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -293,11 +293,22 @@ export class FormComponent implements OnInit {
 		}
 		else if (this.activatedRoute.snapshot.routeConfig.path.includes('publishing-request')) {
 			this.activatedRoute.paramMap.subscribe((params) => {
-				let path = '/chief-editor/publishing-request/' + params.get('id');
+				let role = this.authService.getRole();
+				if(role == "ROLE_CHIEF_EDITOR"){
+					let path = '/chief-editor/publishing-request/' + params.get('id');
 
 				this.router.navigateByUrl('/chief-editor/publishing-requests', { skipLocationChange: true }).then(() => {
 					this.router.navigate([path]);
 				});
+				}
+				else if(role = "ROLE_LECTOR") {
+					let path = '/lector/lector-request/' + params.get('id');
+
+				this.router.navigateByUrl('/lector/lector-requests', { skipLocationChange: true }).then(() => {
+					this.router.navigate([path]);
+				});
+				}
+				
 			});
 
 
